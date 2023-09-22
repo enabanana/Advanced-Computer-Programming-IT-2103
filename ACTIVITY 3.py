46355c6c8cc476cf7d639878d641997897cfe026
Marjane Pindulas <145733322+enabanana@users.noreply.github.com>
2023-09-22 11:22:22 +0800

    Add files via upload

---
+++ ACTIVITY 3.py
@@ -0,0 +1,24 @@
+a = 0
+while a < 5 :
+    print ("Advanced Computer Programming")
+    a = a + 1
+    
+b = 0 
+while b < 10:
+    b += 1
+    print (b)
+    
+c = -1 
+while c < 6:
+    c += 1
+    print (c)
+    
+d = -1 
+while d<9:
+    d += 1
+    print (d)
+    
+name = input("Enter your name: ")
+for _in range (4):
+    print (name)
+    
