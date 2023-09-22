9bbbd5642a9cff8964818041cbded1a5cd2af86e
Marjane Pindulas <145733322+enabanana@users.noreply.github.com>
2023-09-22 11:33:37 +0800

    Add files via upload

---
+++ ACTIVITY 2(AVG).py
@@ -0,0 +1,10 @@
+eng = eval(input("English: "))
+fil = eval(input("Filipino: "))
+sci = eval(input("Science: "))
+ma = eval(input("Math: "))
+ap = eval(input("Araling Panlipunan: "))
+pe = eval(input("Physical Education: "))
+
+average = eng+fil+sci+ma+ap+pe
+total + average/6
+print("Your total average is ", total)
