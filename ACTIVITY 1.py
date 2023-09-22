b10788bd5112e2cb509573b1c869cc41da344657
Marjane Pindulas <145733322+enabanana@users.noreply.github.com>
2023-09-22 10:56:42 +0800

    Add files via upload

---
+++ ACTIVITY 1.py
@@ -0,0 +1,15 @@
+name = str (input("Enter your name:"))
+age = int (input("Enter your age:"))
+address = str (input("Enter your address:"))
+
+print("Your name is:",name)
+print("Your age is:",age)
+print("Your address is:",address)
+
+
+
+
+current=int(input("Enter the current year:"))
+birth=int(input("Enter your birth year:"))
+age=current-birth
+print("Your age is:",age)
