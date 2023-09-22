2bc3852d7288b712715cf4c218d7e7a400e23e4a
Marjane Pindulas <145733322+enabanana@users.noreply.github.com>
2023-09-22 11:27:35 +0800

    Add files via upload

---
+++ ACTIVITY 4.py
@@ -0,0 +1,34 @@
+text = "Hello Peeps!" 
+print (text) 
+text = "This is from a line of code" 
+print (text) 
+print ("5")
+print ("5.5") 
+print ("Hello World!")
+num1 = 40
+num2 = 39
+sum = num1 + num2 
+print(sum) 
+a = 7 
+product = 5*a
+print (product)
+a = 2 
+b = 11 
+a = a*2 
+c = a*b
+b = b+(c*3)
+print (c) 
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+sum = num1 + num2
+print(sum) 
+squared = 3 ** 2 
+cubed = 3 ** 3
+print("Squared:", squared)
+print("Cubed:", cubed) 
+name = "Gly"
+print ("Your name is", name)
+Apple_price = 20 
+orange_price = 20
+result = Apple_price == orange_price
+print(result)
